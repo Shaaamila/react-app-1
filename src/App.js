d241dcddad195266f7831eedfef5cfda9b3aba4b
Shaaamila <shamila.m09@gmail.com>
2022-11-10 17:16:59 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
 function App() {
-  return null;
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  return (
+    <>
+      <TodoList todos = {todos} />
+      <input type="text" />
+      <button>Add Todo</button>  
+      <button>Clear Completed Todos</button>     
+      <div>0 left todo</div>
+      
+    </>
+  );
 }
 
 export default App;
