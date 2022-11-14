437277a5862bf1a36b482a2c5a476d0f89a97393
Shaaamila <shamila.m09@gmail.com>
2022-11-13 16:17:00 -0800

    Completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState([{id: uuidv4(), name: 'Todo1', complete: true}])
+  const [todos, setTodos] = useState([])
 
   const todoNameRef = useRef()
 
@@ -18,24 +18,32 @@ function App() {
      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
-
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find((todo)=> todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if (name === "") return
     setTodos(prevTodos =>{
-      return [...prevTodos, {id:1, name: name, complete:false}]
+      return [...prevTodos, {id:uuidv4(), name: name, complete:false}]
     })
     todoNameRef.current.value = null
   }
-
+  function handleClearTodos(){
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   return (
     <> 
-      <TodoList todos = {todos} />
+      <TodoList todos = {todos} toggleTodo = {toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>  
-      <button>Clear Completed Todos</button>     
-      <div>0 left todo</div>
+      <button onClick={handleClearTodos}>Clear Completed Todos</button>     
+      <div>{todos.filter(todo => !todo.complete).length} left todo</div>
       
     </>
   );
